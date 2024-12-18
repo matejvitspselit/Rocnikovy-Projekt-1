df0a1c6b5263d3f7774527084c38546220f6d9f5
matejvitspselit <vitma1@spselit.cz>
2024-12-18 09:03:01 +0100

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider} from "firebase/auth"
 
 const firebaseConfig = {
-  apiKey: "***************************",
-  authDomain: "apple-redesign-a8dbb.firebaseapp.com",
-  projectId: "apple-redesign-a8dbb",
-  storageBucket: "apple-redesign-a8dbb.appspot.com",
-  messagingSenderId: "857996286829",
-  appId: "1:857996286829:web:a042cd25d139409f2d1864"
+  
 };
 
 const app = initializeApp(firebaseConfig);
